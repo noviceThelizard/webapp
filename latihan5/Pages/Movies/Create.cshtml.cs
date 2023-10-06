90a3bd16e825a66c5e184ffc293e285470129db0
Schaquille Devlin Aristano <116550583+noviceThelizard@users.noreply.github.com>
2023-10-06 23:19:42 +0700

    Add files via upload

---
+++ latihan5/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using latihan5.Data;
+using latihan5.Models;
+
+namespace latihan5.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly latihan5.Data.latihan5Context _context;
+
+        public CreateModel(latihan5.Data.latihan5Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Movie == null || Movie == null)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ latihan5/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using latihan5.Data;
+using latihan5.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace latihan5.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly latihan5.Data.latihan5Context _context;
+
+        public IndexModel(latihan5.Data.latihan5Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get; set; } = default!;
+        public SelectList Genres;
+        public string MovieGenre { get; set; }
+        public async Task OnGetAsync(string movieGenre, string searchString)
+        {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
