baba15473118b2fb9e26b51e80b5bcb9f7dde076
Schaquille Devlin Aristano <116550583+noviceThelizard@users.noreply.github.com>
2023-10-06 23:21:41 +0700

    Add files via upload

---
+++ latihan5/Data/latihan5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using latihan5.Models;
+
+namespace latihan5.Data
+{
+    public class latihan5Context : DbContext
+    {
+        public latihan5Context (DbContextOptions<latihan5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<latihan5.Models.Movie> Movie { get; set; } = default!;
+    }
+}
